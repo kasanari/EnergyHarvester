5019b6f18cfff7a3230d6ee37e165bf0c95a56ef
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-02-19 18:26:36 +0100

    add classes to represent nodes

---
+++ data_collector/node_manager.py
@@ -0,0 +1,17 @@
+
+class Node:
+    def __init__(self, id, energy_level = 0):
+        self.id = id
+        self.energy_level = energy_level
+
+class NodeManager:
+
+    def __init__(self):
+        self.number_of_nodes = 0
+        self.nodes = []
+    
+    def remove_node(id):
+        pass
+
+    def add_node(id):
+        pass
