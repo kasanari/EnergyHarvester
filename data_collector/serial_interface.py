02aa36f24e43d787c5fb78fc2037623273816528
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-02-21 11:24:57 +0100

    add support for headers

--- data_collector/serial_interface.py
+++ data_collector/serial_interface.py
@@ -1,8 +1,18 @@
 import serial, time
 import serial_utils
 
+"""Node actions"""
+GATHER = 1
+SLEEP = 2
+CHARGE = 3
+
+"""Headers"""
+ADD_NODE_HEADER = "add_node"
+DATA_HEADER = "data"
+REMOVE_NODE_HEADER = "remove_node"
+
 def read_from_serial_forever():
-    ser = serial_init()
+    ser = serial_utils.serial_init()
 
     adc_values = []
 
@@ -10,7 +20,7 @@ def read_from_serial_forever():
         line = ser.read_until()
         value = int(line[0:-1])
         print(value)
-    adc_values.append(value)
+        adc_values.append(value)
     ser.close()
 
 def send_action(node_id, action):
@@ -21,11 +31,42 @@ def send_action(node_id, action):
     ser.write(encoded_string)
     ser.close()
 
-def receive_data():
+def receive_message():
     ser = serial_init()
     line = ser.read_until()
     ser.close()
-    return line
+    return parse_msg(line)
+
+def parse_msg(msg):
+    result = msg.split(',')
+    header = None
+    try:
+        header = result[0]
+    except ValueError as error:
+        print("invalid ID character")
+
+    if header is not None:
+        node = parse_data(result[1:])
+
+    return header, node
+
+
+def parse_data(data_string):
+    result = data_string.split(',')
+    node_id = None
+    energy_level = None
+    try:
+        node_id = int(result[0])
+    except ValueError as error:
+        print("invalid ID character")
+    try:
+        energy_level = float(result[1])
+    except ValueError as error:
+        print("invalid data characters")
+
+    if ((node_id is not None) and (energy_level is not None)):
+        return Node(node_id, energy_level)
+
 
 
 
