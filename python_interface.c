bfe0d7c9f8f40100c324291d358fb3bdc7b195e0
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-02-21 11:24:30 +0100

    change messages to use headers

--- python_interface.c
+++ python_interface.c
@@ -3,5 +3,18 @@
 #include "python_interface.h"
 
 void send_data_to_computer(data_t data) {
-    printf("%d, %d\n", data.node_id, data.energy_value);
+    send_data_to_computer(DATA_HEADER, data);
+}
+
+void send_msg_to_computer(char* header, data_t data) {
+     printf("%s, %d, %d\n", header, data.node_id, data.energy_value);
+}
+
+void add_node_to_computer(data_t data) {
+    send_msg_to_computer(ADD_NODE_HEADER, data);
+}
+
+void remove_node_from_computer(int id) {
+    data_t data = {id, 0};
+    send_msg_to_computer(REMOVE_NODE_HEADER, data)   
 }
--- python_interface.h
+++ python_interface.h
@@ -3,6 +3,10 @@
 
 string format = ""
 
+const char *DATA_HEADER = "data"; // header to indicate that data follows
+const char *ADD_NODE_HEADER = "add_node"; // header to indicate that the following number is the id of a node to add
+const char *REMOVE_NODE_HEADER = "remove_node"; // header to indicate that the following number is the id of a node to remove
+
 typedef enum action{CHARGE = 1, SLEEP = 2, GATHER = 3, INVALID = 4} action_t;
 
 typedef struct python_msg { // struct which describes the parts of a message recieved from the computer
