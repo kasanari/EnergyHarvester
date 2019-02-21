a703527aba3f34da41cd34e59337d35bf69ba683
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-02-21 11:23:15 +0100

    add main file

---
+++ data_collector/main.py
@@ -0,0 +1,17 @@
+import data_collector
+import serial_interface
+from node_manager import NodeManager, Node
+from serial_interface import ADD_NODE_HEADER, DATA_HEADER, REMOVE_NODE_HEADER
+
+
+manager = NodeManager()
+
+while(1):
+
+    header, node = serial_interface.receive_message()
+    if header == DATA_HEADER:
+        pass
+    elif header == ADD_NODE_HEADER:
+        manager.add_node(node)
+    elif header == REMOVE_NODE_HEADER:
+        manager.remove_node(node.id)
