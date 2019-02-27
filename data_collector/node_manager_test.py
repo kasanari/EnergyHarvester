3949b397f3a8571bdfc083a48c634c8d38821ff3
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-02-27 16:15:56 +0100

    updated node manager test

--- data_collector/node_manager_test.py
+++ data_collector/node_manager_test.py
@@ -1,4 +1,5 @@
 from node_manager import NodeManager, Node
+import time
 
 manager = NodeManager()
 
@@ -9,14 +10,15 @@ print("Add nodes")
 manager.add_node(node1)
 manager.add_node(node2)
 
-print(manager.nodes)
+print(manager.history)
 
 node1.energy_level = 2
 
 print("Update the energy value of a node")
 manager.update_node(node1)
+manager.commit(time.time())
 
-print(manager.nodes)
+print(manager.history)
 
 print("Check if node 1 is in the system")
 print(manager.node_is_in_system(node1.node_id))
@@ -24,4 +26,3 @@ print("Check if node with id 3 is in the system")
 print(manager.node_is_in_system(3))
 
 
-manager.plot_nodes()
