a598663dc4c096054cf4dec8bea59d77019a40bf
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-03-03 10:49:11 +0100

    implement a (dumb) machine learning algorithm

--- data_collector/machine_learning.py
+++ data_collector/machine_learning.py
@@ -1,69 +1,60 @@
-import gym
+from keras.models import Sequential, InputLayer
+from keras.layers import Dense
+from simulator import Simulation
+from node_manager import Node, Action
+import matplotlib.pyplot as plt
 import numpy as np
-import random
-import tensorflow as tf
 
-env = gym.make('FrozenLake-v0')
 
-tf.reset_default_graph()
+actions = [Action.GATHER, Action.CHARGE]
 
-#These lines establish the feed-forward part of the network used to choose actions
-inputs1 = tf.placeholder(shape=[1, 16], dtype=tf.float32)
-W = tf.Variable(tf.random_uniform([16, 4], 0, 0.01))
-Qout = tf.matmul(inputs1, W)
-predict = tf.argmax(Qout, 1)
+nodes = []
+nodes.append(Node(1, 5))
+nodes.append(Node(2, 5))
+nodes.append(Node(3, 5))
 
-#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
-nextQ = tf.placeholder(shape=[1, 4], dtype=tf.float32)
-loss = tf.reduce_sum(tf.square(nextQ - Qout))
-trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-updateModel = trainer.minimize(loss)
+model = Sequential()
+model.add(InputLayer(batch_input_shape=(1, len(nodes))))
+model.add(Dense(5, activation='sigmoid'))
+model.add(Dense(len(actions), activation='linear'))
+model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+# Q-learning
 
-init = tf.initialize_all_variables()
+num_episodes = 1000
+y = 0.95
+eps = 0.5 # Exploration rate
+r_avg_list = []
+for i in range(num_episodes):
+    if i % 100 == 0:
+        print("Episode {} of {}".format(i + 1, num_episodes))
+    done = False
+    r_sum = 0
+    sim = Simulation(nodes)
+    s = sim.get_state()
 
-init = tf.initialize_all_variables()
+    while not done:
 
-# Set learning parameters
-y = .99
-e = 0.1
-num_episodes = 2000
+        if np.random.random() < eps:
+            a = np.random.randint(0, len(actions))      # Explore by picking a random action
+        else:
+            a = np.argmax(model.predict(s))             # Use network to predict which action to take
 
-#create lists to contain total rewards and steps per episode
-jList = []
-rList = []
+        action = actions[a]
+        #print(action)
+        new_s, r, done = sim.step(action)         # Use selected action to update the environment
+        target = r + y * np.max(model.predict(new_s))   # Calculate Q-value based on new state
+        target_vec = model.predict(s)[0]                # Get both Q-values for this state
+        target_vec[a] = target                          # Update Q-value for the action we took
 
-with tf.Session() as sess:
-    sess.run(init)
-    for i in range(num_episodes):
-        #Reset environment and get first new observation
-        s = env.reset()
-        rAll = 0
-        d = False
-        j = 0
-        #The Q-Network
-        while j < 99:
-            j+=1
-            #Choose an action by greedily (with e chance of random action) from the Q-network
-            a, allQ = sess.run([predict,Qout], feed_dict={inputs1:np.identity(16)[s:s+1]})
-            if np.random.rand(1) < e:
-                a[0] = env.action_space.sample()
-            #Get new state and reward from environment
-            s1,r,d,_ = env.step(a[0])
-            #Obtain the Q' values by feeding the new state through our network
-            Q1 = sess.run(Qout,feed_dict={inputs1:np.identity(16)[s1:s1+1]})
-            #Obtain maxQ' and set our target value for chosen action.
-            maxQ1 = np.max(Q1)
-            targetQ = allQ
-            targetQ[0,a[0]] = r + y*maxQ1
-            #Train our network using target and predicted Q values
-            _,W1 = sess.run([updateModel,W],feed_dict={inputs1:np.identity(16)[s:s+1],nextQ:targetQ})
-            rAll += r
-            s = s1
-            if d == True:
-                #Reduce chance of random action as we train the model.
-                e = 1./((i/50) + 10)
-                break
-        jList.append(j)
-        rList.append(rAll)
-        
-print ("Percent of succesful episodes: " + str(sum(rList)/num_episodes) + "%")
+        model.fit(s, target_vec.reshape(-1, 2), epochs=1, verbose=0) # Fit neural network to predict the Q-values
+
+
+        #print(s)
+        s = new_s # Update state
+        r_sum += r # Add reward to sum of rewards
+
+    r_avg_list.append(r_sum)
+
+plt.figure()
+plt.plot(r_avg_list)
+plt.show()
--- data_collector/simulator.py
+++ data_collector/simulator.py
@@ -11,29 +11,44 @@ class Simulation:
             self.node_manager.add_node(node)
             self.node_manager.update_node(node)
         self.node_manager.commit(time.time())
+        self.steps = 0
 
-    def step(self, actions):
+    def step(self, action):
+        self.steps += 1
         for index, node in enumerate(self.nodes):
-            new_energy_value = node.update(actions[index])
+            new_energy_value = node.update(action)
             if (new_energy_value is not None):
                 self.node_manager.update_node(node)
         self.node_manager.commit(time.time())
 
         new_state = self.get_state()
-        reward = self.calculate_reward(new_state)
+        reward = self.calculate_reward(new_state, action)
         done = self.isDone()
         return new_state, reward, done
 
-    def calculate_reward(self, energy_values):
+    def calculate_reward(self, energy_values, action):
         nulls = energy_values.isnull().sum(axis=1).iloc[0]
-        reward = energy_values.mean(axis=1).iloc[0] - 10*nulls
+        # energy_too_low = self.get_state().mean(axis=1).iloc[0] < 1.8
+        for value in energy_values:
+            if energy_values[value].iloc[0] < 1.8:
+                return -100
+
+        if (nulls > 0):
+            reward = -100
+        elif (action is Action.CHARGE):
+            reward = -1
+        elif (action is Action.GATHER):
+            reward = 1
+        else:
+            reward = 0
         return reward
 
     def get_state(self):
         return self.node_manager.get_most_recent_state().fillna(value=0)
 
     def isDone(self):
-        return self.get_state().isnull().sum(axis=1).iloc[0] == len(self.get_state().columns)
+        energy_too_low = (self.get_state().mean(axis=1).iloc[0] < 1.8)
+        return energy_too_low or (self.steps > 10000)
 
 # nodes = []
 # nodes.append(Node(1, 5))
