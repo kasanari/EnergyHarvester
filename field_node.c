45eb68b8d50782a77e8448486b1db3a830752cd9
Aakash Goel <akashonnet@gmail.com>
2019-03-11 18:51:30 +0100

    Cleaned the code and setup communication with fog node

--- field_node.c
+++ field_node.c
@@ -28,13 +28,16 @@ int rime_channel = CLICKER_CHANNEL;
 int ieee_channel = IEEE802_15_4_CHANNEL;
 linkaddr_t bc_addr;
 
+int count = 4100;
+
+
 
 //message_t message;
 
 PROCESS(client_process, "Main Process");
 //PROCESS(energy_reading, "Energy Reader");
 //PROCESS();
-//, &energy_reading
+//
 AUTOSTART_PROCESSES(&client_process);
 
 
@@ -43,16 +46,11 @@ AUTOSTART_PROCESSES(&client_process);
 
 static void recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
-  	/*int message_ptr;
-	message_ptr = packetbuf_dataptr();
-	if(&message_ptr == 1);
-	{
-		transmit(dataptr);
-	}*/
+  leds_toggle(LEDS_BLUE);	
+	printf("Message recieved");
 	transmit();
-	//broadcast_send(&bc);
 	memcpy(&bc_addr, &from, sizeof(from));
-	leds_toggle(LEDS_BLUE);
+	
 	
 }
 
@@ -83,22 +81,23 @@ static struct broadcast_callbacks bc_callback = {recv};
 //static struct etimer et1, et2, et3, et4;
 void transmit()
 {
-	//static struct etimer et;
-	//etimer_set(&et, CLOCK_SECOND);
-	//packetbuf_copyfrom(rand(), sizeof(int));
-	//packetbuf_copyfrom((void *)dataptr, sizeof(int));
-	int test_message = 5;
-	packetbuf_copyfrom(&test_message, sizeof(int));
-	broadcast_send(&bc);
-	//printf("1 \n");
-	//PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-	//delay(1000);
-	//etimer_reset(&et);
+    status_msg_t status;
+    
+    status.node_id = 11;
+    status.order_number = 1;
+	
+	  //int phidget_value = phidgets.value(PHIDGET3V_2);
+    status.energy_value = 2000;
+		
+		packetbuf_copyfrom(&status, sizeof(status_msg_t));
+		printf("Sent message. id=%d, order_number=%d, energy_value+%d \n", status.node_id, status.order_number, status.energy_value);
+		broadcast_send(&bc);
+		
 }
 
 void sleep()
 {
-
+	
 }
 
 void energised()
@@ -119,7 +118,7 @@ PROCESS_THREAD(client_process, ev, data)
 {
   PROCESS_BEGIN();
  
-  SENSORS_ACTIVATE(phidgets);
+  //SENSORS_ACTIVATE(phidgets); 
 
   broadcast_open(&bc, CLICKER_CHANNEL, &bc_callback);  
   //unicast_open(&uc, CLICKER_CHANNEL, &uc_callback);
@@ -128,24 +127,24 @@ PROCESS_THREAD(client_process, ev, data)
 
   cc2420_set_txpower(CC2420_TX_POWER);
 
-  while(1){
-	static struct etimer et;
-        etimer_set(&et, CLOCK_SECOND);
-        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-
-		int phidget_value = phidgets.value(PHIDGET3V_2);
-
-		/* Copy the adc value into the packet buffer. */
-		packetbuf_copyfrom(&phidget_value, sizeof(int));
-		/* Send the content of the packet buffer using the
-		 * broadcast handle. */
-		//dataptr = &phidget_value;
-		broadcast_send(&bc);
-		printf("%d \n", phidget_value);
-	  }
-	broadcast_close(&bc);
+  PROCESS_YIELD();
+  
+  broadcast_close(&bc);
 PROCESS_END();
 }
-  
+
+/*PROCESS_THREAD(energy_reading, ev, data)
+{
+	PROCESS_BEGIN();
+	while(1)
+	{
+		count -= 100;
+		if(count <= 1500)
+		{
+			
+		}
 
 
+	}
+	PROCESS_END();
+}*/
