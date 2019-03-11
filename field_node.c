4a4c4a6d2092e2cc8faea5c82d032343948a0b47
Aakash Goel <akashonnet@gmail.com>
2019-03-11 15:17:49 +0100

    Sending and recieveing now

--- field_node.c
+++ field_node.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h> 
 #include "contiki.h"
 #include "core/net/rime/rime.h"
 #include "dev/button-sensor.h"
@@ -8,24 +9,27 @@
 #include <stdbool.h>
 #include "network_info.h"
 #include "common.h"
+#include "dev/z1-phidgets.h"
 //#include "python_interface.h"
 
-#define time2 CLOCK_SECOND*2
-#define time1 CLOCK_SECOND
 
 
 int recievedMessage;
-uint16_t energyReading = 100;
+
+int dataptr;
+int energyReading = 100;
 char s_v ='S';
 const uint16_t threshold2 = 80;
 const uint16_t threshold1 = 40;
 const uint16_t threshold0 = 20;		
 bool b_mess = true;
-bool u_mess = true;
+//bool u_mess = true;
 int rime_channel = CLICKER_CHANNEL;
 int ieee_channel = IEEE802_15_4_CHANNEL;
-linkaddr_t bc_addr, uc_addr;
+linkaddr_t bc_addr;
 
+
+//message_t message;
 
 PROCESS(client_process, "Main Process");
 //PROCESS(energy_reading, "Energy Reader");
@@ -34,36 +38,80 @@ PROCESS(client_process, "Main Process");
 AUTOSTART_PROCESSES(&client_process);
 
 
+
+
+
 static void recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
-  	//if message recieved the set b_mess to true
-  	b_mess = true;
-	//bc_addr = from;
+  	/*int message_ptr;
+	message_ptr = packetbuf_dataptr();
+	if(&message_ptr == 1);
+	{
+		transmit(dataptr);
+	}*/
+	transmit();
+	//broadcast_send(&bc);
 	memcpy(&bc_addr, &from, sizeof(from));
-	leds_toggle(0b011);
+	leds_toggle(LEDS_BLUE);
+	
 }
 
 
 
-static void recv_uc(struct unicast_conn *c, const linkaddr_t *from)
+/*static void recv_uc(struct unicast_conn *c, const linkaddr_t *from)
 {
   	//if message recieved then set u_mess to true
   	u_mess = true;
 	memcpy(&uc_addr, &from, sizeof(from));
 	leds_toggle(0b101);
 }
+*/
+ 
 
 static struct broadcast_conn bc;
 
-static struct broadcast_callbacks bc_callback = {recv};  
+static struct broadcast_callbacks bc_callback = {recv};
 
-static struct unicast_conn uc;
+ 
 
-static struct unicast_callbacks uc_callback = {recv_uc};
+//static struct unicast_conn uc;
+
+//static struct unicast_callbacks uc_callback = {recv_uc};
 
 
 //TIMER
-static struct etimer et1, et2, et3, et4;
+//static struct etimer et1, et2, et3, et4;
+void transmit()
+{
+	//static struct etimer et;
+	//etimer_set(&et, CLOCK_SECOND);
+	//packetbuf_copyfrom(rand(), sizeof(int));
+	//packetbuf_copyfrom((void *)dataptr, sizeof(int));
+	int test_message = 5;
+	packetbuf_copyfrom(&test_message, sizeof(int));
+	broadcast_send(&bc);
+	//printf("1 \n");
+	//PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+	//delay(1000);
+	//etimer_reset(&et);
+}
+
+void sleep()
+{
+
+}
+
+void energised()
+{}
+
+void send_window()
+{}
+
+void await_order()
+{}
+
+void exec_order()
+{}
 
 
 
@@ -71,149 +119,33 @@ PROCESS_THREAD(client_process, ev, data)
 {
   PROCESS_BEGIN();
  
-  broadcast_open(&bc, CLICKER_CHANNEL, &bc_callback);
-  unicast_open(&uc, CLICKER_CHANNEL, &uc_callback);
+  SENSORS_ACTIVATE(phidgets);
+
+  broadcast_open(&bc, CLICKER_CHANNEL, &bc_callback);  
+  //unicast_open(&uc, CLICKER_CHANNEL, &uc_callback);
 
   cc2420_set_channel(IEEE802_15_4_CHANNEL);
 
   cc2420_set_txpower(CC2420_TX_POWER);
 
   while(1){
-switch(s_v)
-  {
-    /*When the energy level is below the threshold t1 the device will be in the
-      Sleep State where s_v = S*/
-  case 'S':
-    {
-      if(energyReading>threshold2)      //Energy levels rise above t1 then execute the
-	{
-	  s_v = 'E';
-	  printf("From Sleeping state to energized state \n");
-	}
-      else 
-	{
-	  //do nothing
-	}
-      break;
-    }
-  case 'E':
-    {
-      if(energyReading>threshold1)
-	{
-	  //setup RIME buffer and check if anything has been recieved
-	  if(b_mess)
-	    {
-	      s_v = 'W';
-	      printf("From energized state to sending window state \n");
-	    }
-	  else
-	    {
-	    //do nothing
-	    }
-	}
-      else
-	{
-	  s_v = 'S';
-	  printf("From energized state to sleep state \n");
-	}
-      b_mess = false;
-      break;
-    }
-  case 'W':
-    {
-      etimer_set(&et1, CLOCK_SECOND);
-      if(energyReading>threshold0)
-	{
-	  unicast_send(&uc, &uc_addr);
-	  //send information including energy level
-	  if(1)      //if message sent successfully and timer not expired
-	    {
-	      s_v = 'A';
-	      printf("From sending window state to awaiting order \n");
-	    }
-	  else if(etimer_expired(&et1))       //timer expires AND no message sent
-	    {
-	      //resend message
-	    }
-	}
-      else
-	{
-	  s_v = 'S';
-	  printf("From sending window to sleep state \n");
-	}
-      break;
-    }
-  case 'A':
-    {
-      if(energyReading>threshold0)
-	{
-	  //check if order has been recieved
-	  if(u_mess)                 //message recieved
-	    {
-	      s_v = 'O';
-	      printf("From awaiting order to executing order \n");
-	    }
-	  else
-	    {
-	      //do nothing
-	    }
-	}
-      else
-	{
-	  s_v = 'S';
-	  printf("From awaiting awaiting order to sleep state \n");
-	}
-      break;
-    }
-  case 'O':
-    {
-      action_t order = 1;
-      if(energyReading>threshold0)
-	{
-	  //start timer
-	  //execute order
-	  if(order == 1)
-	    {
-	      //perform action charge
-	      printf("Order number 1 will be executed \n");
-	      s_v = 'S';
-	      unicast_send(&uc, &uc_addr);
-	      
-	    }
-	  else if(order == 2)
-	    {
-	      //perform action goto sleep
-	      s_v = 'S';
-	      printf("Order number 2 being executed \n");
-	      unicast_send(&uc, &uc_addr);
-	      order = 3;
-	      
-	    }
-	  else if(order == 3)
-	    {
-	      //perform action gather data and TRANSMIT
-	      printf("Order number 3 is being executed \n");
-	      unicast_send(&uc, &uc_addr);
-	      order = 4;
-	      
-	    }
-	  else if(order == 4)
-	    {
-	      //perform action invalid
-	      printf("Invalid order number. Please specify order number between 1 and 3 \n");
-	      order = 0;
-	      
-	    }
-	}
-      else
-	{
-	  s_v = 'S';
-	  printf("From executing order to sleep state \n");
-	}
-      break;
-    }
-  }
-  }
-  PROCESS_END();
+	static struct etimer et;
+        etimer_set(&et, CLOCK_SECOND);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
+		int phidget_value = phidgets.value(PHIDGET3V_2);
+
+		/* Copy the adc value into the packet buffer. */
+		packetbuf_copyfrom(&phidget_value, sizeof(int));
+		/* Send the content of the packet buffer using the
+		 * broadcast handle. */
+		//dataptr = &phidget_value;
+		broadcast_send(&bc);
+		printf("%d \n", phidget_value);
+	  }
+	broadcast_close(&bc);
+PROCESS_END();
 }
+  
+
+
